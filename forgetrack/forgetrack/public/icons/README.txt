b1c816936afd6716f0c4d576af4a213bd662512f
forgeit-co-in <forgeit.co@gmail.com>
2026-09-26 10:49:42 +0530

    Delete forgetrack/forgetrack directory

--- forgetrack/forgetrack/public/icons/README.txt
+++
@@ -1,3 +0,0 @@
-Place icon-192.png and icon-512.png (square, PNG) in this folder before building the PWA.
-Generate them from public/favicon.svg using any icon generator, e.g.:
-npx pwa-asset-generator public/favicon.svg public/icons
--- forgetrack/forgetrack/src/services/departments.ts
+++
@@ -1,7 +0,0 @@
-import { supabase } from '@/lib/supabase'
-import { Department } from '@/types'
-
-export async function getDepartments() {
-  const { data, error } = await supabase.from('departments').select('*').order('name')
-  return { data: (data ?? []) as Department[], error }
-}
